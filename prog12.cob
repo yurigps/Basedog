4ec6e1629999f0b4b34bb2f5ca614c5a2dfe14ed
YuriGPS <yurigps88@gmail.com>
2026-04-16 00:32:28 -0300

    alteração no layout do arquivo, mudança no tipo do registro CHAVE

--- prog12.cob
+++ prog12.cob
@@ -23,7 +23,7 @@
            05 NOME   PIC X(20).
            05 IDADE  PIC 9(3).
            05 EMAIL  PIC X(20).
-           05 CHAVE PIC X(2).
+           05 CHAVE PIC 9(2).
 
        WORKING-STORAGE SECTION.
        01  EOF          PIC X VALUE "N".
